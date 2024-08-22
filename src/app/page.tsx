64b23de114edd45616aa9395b5b5dd66a42f0215
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-23 00:40:31 +0300

    chore: configure main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import styles from "./page.module.css";
 import Form from "@/app/components/Form/Form";
+import Page from "@/app/pages/DataFetcher/page";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Form />
+        <Page />
     </main>
   );
 }
