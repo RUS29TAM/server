4c82a459dee711e9d5a946416fa1c82dcbb2e1e0
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-24 12:50:16 +0300

    chore: add del func for each elem

--- src/app/pages/DataFetcher/page.tsx
+++ src/app/pages/DataFetcher/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import styles from './DataFetcher.module.css';
+import Link from "next/link";
 
 interface DataItem {
     _id?: string;
@@ -30,11 +31,22 @@ const Page: React.FC = () => {
         }
     };
 
+    const deleteData = async (id: string) => {
+        try {
+            await axios.delete(`http://localhost:5000/api/data/${id}`);
+            // Удаление элемента из состояния после успешного удаления на сервере
+            setData(data.filter(item => item._id !== id));
+        } catch (err) {
+            setError('Ошибка при удалении данных');
+        }
+    };
+
     return (
         <div className={styles.dataFetcher}>
             <button className={styles.loadButton} onClick={fetchData} disabled={loading}>
                 {loading ? 'Загрузка...' : 'Загрузить данные'}
             </button>
+
             {error && <div className={styles.error}>{error}</div>}
             <div className={styles.dataContainer}>
                 {data.map((item) => (
@@ -43,9 +55,18 @@ const Page: React.FC = () => {
                         <p><strong>Возраст</strong> {item.age} &nbsp; лет</p>
                         <p><strong>Слово</strong> {item.word}</p>
                         <p><strong>Значение</strong> {item.description}</p>
+                        <button
+                            className={styles.deleteButton}
+                            onClick={() => item._id && deleteData(item._id)}
+                        >
+                            Удалить
+                        </button>
                     </div>
                 ))}
             </div>
+            <Link className={styles.loadButton} href={'/'} >
+                {loading ? 'Загрузка...' : 'Вернуться назад'}
+            </Link>
         </div>
     );
 };
