4a7185ec56d245fc921d4b713ebaeee922e2987d
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-26 14:47:20 +0300

    chore: remove commented text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import styles from "./page.module.css";
 import Form from "@/app/components/Form/Form";
-import Page from "@/app/pages/DataFetcher/page";
 
 export default function Home() {
   return (
