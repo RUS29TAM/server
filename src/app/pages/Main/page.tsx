4426140a6612602e840d53f64102eabb080ee6a5
RUS29TAM <rustam.nikonchuk@yandex.ru>
2024-08-31 23:18:23 +0300

    chore: add main page

---
+++ src/app/pages/Main/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import React from 'react';
+import style from './Main.module.css';
+import Link from "next/link";
+
+const Form: React.FC = () => {
+
+    return (
+        <div className={style.wrapper}>
+            <div className={style.container}>
+                <div className={style.box}>
+                    <Link href={'/pages/Form'}>
+                        <h2 className={style.cardHeader}>Пополнить словарь</h2>
+                        <img className={style.img}
+                             alt={'img_unsplash'}
+                             src={'https://irbis128.kraslib.ru/?id=WIrbis&action=FT/GetPageImageThumb&sid=a74f990c671c8ace3379138e009c79f6&page=3'}>
+                        </img>
+                    </Link>
+                </div>
+                <div className={style.box}>
+                    <Link href={'/pages/GuessWord'}>
+                        <h2 className={style.cardHeader}>Угадать слово</h2>
+                        <img className={style.img}
+                             alt={'img_unsplash'}
+                             src={'https://images.unsplash.com/photo-1516246843873-9d12356b6fab?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}>
+                        </img>
+                    </Link>
+                </div>
+                <div className={style.box}>
+                    <Link href={'/pages/DataFetcher'}>
+                        <h2 className={style.cardHeader}>Смотреть словарь</h2>
+                        <img className={style.img}
+                             alt={'img_unsplash'}
+                             src={'https://images.unsplash.com/photo-1511923468502-4e48e59ef6f5?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}>
+                        </img>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Form;
